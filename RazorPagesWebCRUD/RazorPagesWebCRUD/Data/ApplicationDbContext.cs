10ec762990ccf2d288772520a0ceede7e95bcbf6
thoangtran20 <thoangtran20@gmail.com>
2023-12-25 10:21:36 +0700

    Set up project RazorPageCRUD - Create CategoryModel - Add DataAnnotations - Add Connection to project

---
+++ RazorPagesWebCRUD/RazorPagesWebCRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesWebCRUD.Model;
+
+namespace RazorPagesWebCRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+
+}
